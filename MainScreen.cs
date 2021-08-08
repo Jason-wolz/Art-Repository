f972edcaa487e6144b22859759c8ef4ef5643e7a
Jason Wolz <jwolz1@student.wgu.edu>
2021-08-08 16:45:49 -0400

    Add project files.

---
+++ MainScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Capstone_Project
+{
+    public partial class MainScreen : Form
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+    }
+}
